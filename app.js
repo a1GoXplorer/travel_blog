2a9e250e352191128b6c0557b769a7cd60abb806
Mark Daniels <mark_daniels1982@hotmail.com>
2015-02-08 08:59:43 -0800

    add methodOverride, add homepage

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var pg = require('pg');
+var methodOverride = require('method-override');
 
 
 var app = express();
+
+app.set('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+app.get('/', function(req, res) {
+  res.render('site/home.ejs');
+})
 
 
 app.listen(3000, function() {
